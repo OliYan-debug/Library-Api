12513b2a115a96587ae6a6e19ac224b9a364abee
OliYan-debug <77648737+OliYan-debug@users.noreply.github.com>
2023-10-20 18:07:28 -0300

    Initial Commit

---
+++ src/main/java/oliyandebug/santanderbootcampapi/SantanderBootcampApiApplication.java
@@ -0,0 +1,13 @@
+package oliyandebug.santanderbootcampapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderBootcampApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderBootcampApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/oliyandebug/santanderbootcampapi/SantanderBootcampApiApplicationTests.java
@@ -0,0 +1,13 @@
+package oliyandebug.santanderbootcampapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderBootcampApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
