47f396504c692f87cc2ed181b1ad3d20e67e985f
OliYan-debug <77648737+OliYan-debug@users.noreply.github.com>
2023-10-21 10:31:48 -0300

    Add Swagger as dependency and Add dev profile

--- src/main/java/oliyandebug/libraryapi/Application.java
+++ src/main/java/oliyandebug/libraryapi/Application.java
@@ -1,13 +1,13 @@
-package oliyandebug.santanderbootcampapi;
+package oliyandebug.libraryapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SantanderBootcampApiApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SantanderBootcampApiApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
--- src/test/java/oliyandebug/libraryapi/SantanderBootcampApiApplicationTests.java
+++ src/test/java/oliyandebug/libraryapi/SantanderBootcampApiApplicationTests.java
@@ -1,4 +1,4 @@
-package oliyandebug.santanderbootcampapi;
+package oliyandebug.libraryapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
